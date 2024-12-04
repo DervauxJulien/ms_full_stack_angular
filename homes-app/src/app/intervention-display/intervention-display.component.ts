2b77e36267ec20dae689d046ca234b2703298b92
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-12-04 14:59:56 +0100

    new branch pour update

--- homes-app/src/app/intervention-display/intervention-display.component.ts
+++ homes-app/src/app/intervention-display/intervention-display.component.ts
@@ -3,36 +3,27 @@ import { CommonModule } from '@angular/common';
 import { InterventionService } from '../services/intervention.service';
 import { ActivatedRoute } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
-
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-intervention-display',
   standalone: true,
-  imports: [CommonModule, MatTableModule],
+  imports: [CommonModule, MatTableModule, CdkTableModule, MatButtonModule],
   templateUrl: './intervention-display.component.html',
   styleUrls: ['./intervention-display.component.css'],
 })
 export class InterventionDisplayComponent {
   interventionId!: string;
-  loading = true; 
-  interventionData: any[] = []; 
-  displayedColumns: string[] = [
-    'seeMore',
-    'ID_INTERVENTION',
-    'DESCRIPTION',
-    'CREATION_DATE',
-    'STATUS',
-  ]; 
+  loading = true;
+  interventionData: any[] = [];
+  displayedColumns: string[] = ['test', 'ID_INTERVENTION', 'DESCRIPTION', 'CREATION_DATE', 'STATUS'];
+  expandedElement: any | null = null;
 
   constructor(
     private interventionService: InterventionService,
     private route: ActivatedRoute
   ) {}
 
-  toggleDescription(element: any): void {
-    element.showDescription = !element.showDescription;
-  }
-
- 
   ngOnInit() {
     this.interventionId = this.route.snapshot.paramMap.get('id')!;
 
@@ -40,7 +31,7 @@ export class InterventionDisplayComponent {
       this.interventionService.getInterventionById(this.interventionId).subscribe({
         next: (data) => {
           this.interventionData = [data]; 
-          this.loading = false; 
+          this.loading = false;
         },
         error: (error) => {
           console.error('Erreur lors de la récupération des données:', error);
@@ -52,4 +43,10 @@ export class InterventionDisplayComponent {
       this.loading = false;
     }
   }
+
+  toggleDescription(element: any): void {
+    this.expandedElement = this.expandedElement === element ? null : element;
+    console.log(this.expandedElement);
+  }
+  
 }
