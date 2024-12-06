e7987a7069da1aba4e9f625121c4449deabc0ec7
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-12-06 11:58:51 +0100

    creation utils et login

---
+++ homes-app/src/app/components/utils/utils.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-utils',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './utils.component.html',
+  styleUrls: ['./utils.component.css']
+})
+export class UtilsComponent {
+
+}
